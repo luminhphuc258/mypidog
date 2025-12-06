9f2e3822e7215f8032135b753646816bf5537cdc
luminhphuc258 <luminhphuc258@gmail.com>
2025-12-06 11:37:38 -0600

    fix bug unclokc khoi tao pidog4

--- unclock_khoitaoservomotor.py
+++ unclock_khoitaoservomotor.py
@@ -115,7 +115,7 @@ LEG_INIT_ANGLES = [-3, 89, 9, -80, 3, 90, 10, -90]
 # Head theo mapping mới: [NECK_PITCH(P8), NECK_TILT(P9), HEAD_YAW(P10)]
 # Bạn bị "ngã trái 90" là do P9=+90 -> tilt max.
 # Cho đầu thẳng: set tilt = 0
-HEAD_INIT_ANGLES = [90, -90, -29]
+HEAD_INIT_ANGLES = [90, -90, 90]
 
 # Tail: PHẢI là list (số phần tử = số pin tail)
 TAIL_INIT_ANGLE = [0]
