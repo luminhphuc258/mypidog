9ae6ebaa977de6f265bd8fa4da2e90ba68100898
luminhphuc258 <luminhphuc258@gmail.com>
2025-12-06 00:14:15 -0600

    test cam bien gia toc v3

--- test_imu.py
+++ test_imu.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python3
 from pidog import Pidog
+from time import sleep
 
 dog = Pidog()
+sleep(0.3)
 
-print("=== AVAILABLE ATTRIBUTES IN dog.imu ===")
-print(dir(dog.imu))
+print("=== TEST IMU SH3001 (Accel + Gyro + Temp) ===")
+print("Nhấn Ctrl+C để dừng...\n")
+
+while True:
+    data = dog.imu.read()   # CHÍNH XÁC API CỦA PIDOG BẢN NÀY
+    print(data)
+    sleep(0.1)
