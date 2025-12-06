bdc47dfb95d393539af052c51fdb0b1088b83dad
luminhphuc258 <luminhphuc258@gmail.com>
2025-12-06 10:30:36 -0600

    fix bug active listening v92

--- testthuvienphatnhac.py
+++ testthuvienphatnhac.py
@@ -1,51 +1,13 @@
-from robot_hat import Buzzer, Pin, Music
-import time
+from pidog.audio import Audio
+from time import sleep
 
-music = Music()
+audio = Audio()
 
-# Active buzzer only
-buzzer = Buzzer(Pin("D0"))   # ĐỔI D0 → D1 nếu buzzer nằm ở chân khác
+print("Playing...")
+audio.play("testmic.wav")
 
-music.tempo(120, 1/4)
+while audio.is_playing():
+    sleep(0.1)
 
-def beep(note, beat):
-    freq = music.note(note)
-    duration = music.beat(beat)
-
-    buzzer.on()
-    time.sleep(duration)
-    buzzer.off()
-    time.sleep(0.05)
-
-def shark():
-    beep("C5", 1/8)
-    beep("C5", 1/8)
-    beep("C5", 1/8)
-    beep("C5", 1/16)
-    beep("C5", 1/8)
-    beep("C5", 1/16)
-    beep("C5", 1/8)
-
-for _ in range(2):
-    print("Measure 1")
-    beep("G4", 1/4)
-    beep("A4", 1/4)
-
-    print("Measure 2")
-    shark()
-    beep("G4", 1/8)
-    beep("A4", 1/8)
-
-    print("Measure 3")
-    shark()
-    beep("G4", 1/8)
-    beep("A4", 1/8)
-
-    print("Measure 4")
-    shark()
-    beep("C5", 1/8)
-    beep("C5", 1/8)
-
-    print("Measure 5")
-    beep("B4", 1/4)
-    time.sleep(music.beat(1/4))
+audio.stop()
+print("Done.")
