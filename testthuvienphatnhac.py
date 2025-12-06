951283215a652923b541a429652f5d4f5d598951
luminhphuc258 <luminhphuc258@gmail.com>
2025-12-06 10:28:54 -0600

    fix bug active listening v93

--- testthuvienphatnhac.py
+++ testthuvienphatnhac.py
@@ -1,49 +1,51 @@
-# Import Buzzer class
-from robot_hat import Buzzer
-# Import Pin for active buzzer
-from robot_hat import Pin
-# Import PWM for passive buzzer
-from robot_hat import PWM
-# import Music class for tones
-from robot_hat import Music
-# Import time for sleep
+from robot_hat import Buzzer, Pin, Music
 import time
 
 music = Music()
-# Create Buzzer object for passive buzzer
-p_buzzer = Buzzer(PWM(0))
-# Create Buzzer object for active buzzer
-a_buzzer = Buzzer(Pin("D0"))
+
+# Active buzzer only
+buzzer = Buzzer(Pin("D0"))   # ĐỔI D0 → D1 nếu buzzer nằm ở chân khác
 
 music.tempo(120, 1/4)
 
-# Make a Shark-doo-doo function as is all about it
-def shark_doo_doo():
-    p_buzzer.play(music.note("C5"), music.beat(1/8))
-    p_buzzer.play(music.note("C5"), music.beat(1/8))
-    p_buzzer.play(music.note("C5"), music.beat(1/8))
-    p_buzzer.play(music.note("C5"), music.beat(1/16))
-    p_buzzer.play(music.note("C5"), music.beat(1/16 + 1/16))
-    p_buzzer.play(music.note("C5"), music.beat(1/16))
-    p_buzzer.play(music.note("C5"), music.beat(1/8))
+def beep(note, beat):
+    freq = music.note(note)
+    duration = music.beat(beat)
 
-# loop any times you want from baby to maybe great great great grandpa!
-for _ in range(3):
+    buzzer.on()
+    time.sleep(duration)
+    buzzer.off()
+    time.sleep(0.05)
+
+def shark():
+    beep("C5", 1/8)
+    beep("C5", 1/8)
+    beep("C5", 1/8)
+    beep("C5", 1/16)
+    beep("C5", 1/8)
+    beep("C5", 1/16)
+    beep("C5", 1/8)
+
+for _ in range(2):
     print("Measure 1")
-    p_buzzer.play(music.note("G4"), music.beat(1/4))
-    p_buzzer.play(music.note("A4"), music.beat(1/4))
+    beep("G4", 1/4)
+    beep("A4", 1/4)
+
     print("Measure 2")
-    shark_doo_doo()
-    p_buzzer.play(music.note("G4"), music.beat(1/8))
-    p_buzzer.play(music.note("A4"), music.beat(1/8))
+    shark()
+    beep("G4", 1/8)
+    beep("A4", 1/8)
+
     print("Measure 3")
-    shark_doo_doo()
-    p_buzzer.play(music.note("G4"), music.beat(1/8))
-    p_buzzer.play(music.note("A4"), music.beat(1/8))
+    shark()
+    beep("G4", 1/8)
+    beep("A4", 1/8)
+
     print("Measure 4")
-    shark_doo_doo()
-    p_buzzer.play(music.note("C5"), music.beat(1/8))
-    p_buzzer.play(music.note("C5"), music.beat(1/8))
+    shark()
+    beep("C5", 1/8)
+    beep("C5", 1/8)
+
     print("Measure 5")
-    p_buzzer.play(music.note("B4"), music.beat(1/4))
+    beep("B4", 1/4)
     time.sleep(music.beat(1/4))
