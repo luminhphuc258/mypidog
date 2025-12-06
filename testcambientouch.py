8031c838224402bc6750ab71b591cb491abda58c
luminhphuc258 <luminhphuc258@gmail.com>
2025-12-05 21:36:26 -0600

    fix bug sensor  v3

--- testcambientouch.py
+++ testcambientouch.py
@@ -3,15 +3,13 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-PIN_LEFT  = 27   # dây xanh lá
-PIN_RIGHT = 22   # dây vàng
+PIN_LEFT  = 27   # OUT1
+PIN_RIGHT = 22   # OUT2
 
-GPIO.setup(PIN_LEFT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(PIN_RIGHT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PIN_LEFT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PIN_RIGHT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-print("=== TOUCH SENSOR TEST ===")
-print("Chạm vào LEFT hoặc RIGHT để xem giá trị thay đổi")
-print("0 = đang chạm, 1 = không chạm\n")
+print("=== TOUCH TEST PUD_DOWN ===")
 
 try:
     while True:
@@ -19,7 +17,5 @@ try:
         R = GPIO.input(PIN_RIGHT)
         print(f"L={L}  R={R}")
         sleep(0.1)
-
 except KeyboardInterrupt:
     GPIO.cleanup()
-    print("\nThoát.")
