bc1e15b850d650dbdc89214a97ca8cb4e17708ef
luminhphuc258 <luminhphuc258@gmail.com>
2025-12-06 22:08:15 -0600

    suyluan nguoc v13

--- callmatthewsuper.py
+++ callmatthewsuper.py
@@ -118,11 +118,31 @@ def main():
         dog.wait_all_done()
         time.sleep(0.3)
 
+        dog.do_action("forward", speed=30)
+        dog.wait_all_done()
+        time.sleep(0.3)
+
+        dog.do_action("backward", speed=30)
+        dog.wait_all_done()
+        time.sleep(0.3)
+
+        dog.do_action("turn_right", speed=30)
+        dog.wait_all_done()
+        time.sleep(0.3)
+
+        dog.do_action("stand", speed=1)
+        dog.wait_all_done()
+        time.sleep(0.3)
+
+        dog.do_action("trot", speed=20)
+        dog.wait_all_done()
+        time.sleep(0.3)
+
     finally:
         # Step 4) return to pose from file (robot_hat only) then close
         print("[STEP4] Return to config pose then exit.")
       
-        #apply_pose_config(cfg, step_delay=DELAY_BETWEEN_WRITES, settle_sec=1.0)
+  
 
 if __name__ == "__main__":
     main()
