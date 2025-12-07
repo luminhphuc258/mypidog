3758d6365f83d2a071706fd030602d0dcefdfabd
luminhphuc258 <luminhphuc258@gmail.com>
2025-12-06 19:47:35 -0600

    suyluan nguoc v5

---
+++ callmatthewadvanced.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import time
+from matthewpidogclassinit import MatthewPidogBootClass
+from pidog.preset_actions import push_up
+
+def main():
+    boot = MatthewPidogBootClass()
+    dog = boot.create()  # khởi tạo xong
+
+    # cho servo ổn định chút để đỡ té
+    time.sleep(1.0)
+
+    # push up (3 lần)
+    push_up(dog, speed=92)
+    dog.wait_all_done()
+    time.sleep(0.3)
+
+    # stand
+    dog.do_action("stand", speed=92)
+    dog.wait_all_done()
+    time.sleep(0.3)
+
+    # sit
+    dog.do_action("sit", speed=92)
+    dog.wait_all_done()
+
+if __name__ == "__main__":
+    main()
