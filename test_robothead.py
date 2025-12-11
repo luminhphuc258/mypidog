648020d36885932a85bc5087049717779f690d9a
luminhphuc258 <luminhphuc258@gmail.com>
2025-12-11 13:39:54 -0600

    update xoay head v7

--- test_robothead.py
+++ test_robothead.py
@@ -12,7 +12,7 @@ from matthewpidogclassinit import MatthewPidogBootClass
 
 POSE_FILE = Path(__file__).resolve().parent / "pidog_pose_config.txt"
 SERVO_PORTS = [f"P{i}" for i in range(12)]  # P0..P11
-DELAY_BETWEEN_WRITES = 1.0   # bước nhỏ để đi rất chậm
+DELAY_BETWEEN_WRITES = 0.2   # bước nhỏ để đi rất chậm
 SETTLE_SEC = 1.0
 ANGLE_MIN, ANGLE_MAX = -90, 90
 
